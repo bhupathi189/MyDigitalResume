26d4ec545b72373269c1fdd5025d56718ef7f4bf
bhupathi189 <pattapagalubhupathi@outlook.com>
2020-11-08 11:53:23 +0530

    08-11-2020

---
+++ precache-manifest.716942293ee38674ad8cb58811dc593d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bc3141717849650fdabb9eb417c5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb213d4ac71d1dcd1b40",
+    "url": "/static/css/main.e954a3d0.chunk.css"
+  },
+  {
+    "revision": "e66293db39e4f9825549",
+    "url": "/static/js/2.9866a256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9866a256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb213d4ac71d1dcd1b40",
+    "url": "/static/js/main.602c93b2.chunk.js"
+  },
+  {
+    "revision": "5a70e8d867a4328b3413",
+    "url": "/static/js/runtime-main.e0a81464.js"
+  }
+]);
